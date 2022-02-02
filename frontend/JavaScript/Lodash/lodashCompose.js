5054c9e2d7295eeb6ccf38918a64677d5ac42a9b
booding <booding.xiu@outlook.com>
2022-02-02 21:01:43 +0800

    add: add lodash compose

---
+++ frontend/JavaScript/Lodash/lodashCompose.js
@@ -0,0 +1,29 @@
+const _ = require('lodash')
+
+const reverse = arr => arr.reverse();
+const first = arr => arr[0];
+const toUpper = s => s.toUpperCase();
+
+const f = _.flowRight(toUpper, first, reverse)
+console.log(f(['one', 'two', 'three']))
+
+
+// realization
+
+function compose(...args) {
+    return function (value) {
+        return args.reverse().reduce(function (previousResult, func) {
+            return func(previousResult)
+        }, value)
+    }
+}
+
+let myF = compose(toUpper, first, reverse)
+console.log(myF(['one', 'two', 'three']))
+
+// ES6
+
+let composeES6 = (...args) => value => args.reverse().reduce((previousResult, func) => func(previousResult), value)
+
+let myFES6 = composeES6(toUpper, first, reverse)
+console.log(myFES6(['one', 'two', 'three']))
