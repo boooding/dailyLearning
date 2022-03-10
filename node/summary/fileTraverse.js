f88f78e755f57873ef65ed952ea1ec9b4fd15e5d
booding <booding.xiu@outlook.com>
2022-03-10 14:11:15 +0800

    add: add fileTraverse

---
+++ node/summary/fileTraverse.js
@@ -0,0 +1,41 @@
+// const path = require('path');
+// const fs = require('fs')
+//
+// const pathName =  path.basename(__filename)
+//
+// console.log(pathName)
+// fs.readFileSync()
+//
+// function simulatedBasename(pathName) {
+//     return pathName.split('/').reverse()[0];
+// }
+//
+// console.log(simulatedBasename(__filename))
+
+const fs = require("fs");
+const path = require("path");
+const fileTraverse = (filePath, operationToFile) => {
+    fs.readdir(filePath, (err, files) => {
+        if (err) {
+            return console.warn(err)
+        }
+        files.forEach((file) => {
+            const fileDir = path.join(filePath, file);
+            fs.stat(fileDir, (err, stats) => {
+                if (err) {
+                    console.warn('failed in get stats')
+                } else {
+                    if (stats.isFile()) {
+                        operationToFile(fileDir)
+                    } else if (stats.isDirectory()) {
+                        fileTraverse(fileDir)
+                    }
+
+                }
+            })
+        })
+    })
+}
+fileTraverse(__dirname, (fileDir) => {
+    console.log(fileDir)
+})
